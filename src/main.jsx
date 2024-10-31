bfd2404e2c536588571a4f5d7f9d62cbfb7d77a9
Bethel <bethelededh668@gmail.com>
2024-10-31 16:10:11 -0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import router from './Router/Router.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+
+)
